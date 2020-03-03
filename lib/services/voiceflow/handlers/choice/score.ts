96c10bc43b3fc1f2f16d152e69d1f2bff1fd136e
leartgjoni-voiceflow <leart@voiceflow.com>
2020-03-03 16:49:25 -0500

    chore: make code easier

--- lib/services/voiceflow/handlers/choice/score.ts
+++ lib/services/voiceflow/handlers/choice/score.ts
@@ -6,32 +6,27 @@ type Choice = { value: string; index: number };
 const getBestScore = (input: string, choices: Array<Choice>, tolerance = 0.9): number | null => {
   if (!input) return null;
 
-  const best = choices.reduce(
-    (acc: { choice: null | Choice; score: number }, choice) => {
-      if (!choice.value) return acc;
+  const best: { choice: null | Choice; score: number } = {
+    choice: null,
+    score: tolerance,
+  };
 
-      const sanitizedInput = input.toLowerCase();
-      const match = choice.value.toLowerCase();
-      let tempScore = jaccard(sanitizedInput, match) + dice(sanitizedInput, match);
+  choices.forEach((choice) => {
+    if (!choice.value) return;
 
-      if (input.charAt(0) === match.charAt(0)) {
-        tempScore += 0.1;
-      }
+    const sanitizedInput = input.toLowerCase();
+    const match = choice.value.toLowerCase();
+    let tempScore = jaccard(sanitizedInput, match) + dice(sanitizedInput, match);
 
-      if (tempScore > acc.score) {
-        acc = {
-          score: tempScore,
-          choice,
-        };
-      }
-
-      return acc;
-    },
-    {
-      choice: null,
-      score: tolerance,
+    if (input.charAt(0) === match.charAt(0)) {
+      tempScore += 0.1;
     }
-  );
+
+    if (tempScore > best.score) {
+      best.choice = choice;
+      best.score = tempScore;
+    }
+  });
 
   return best.choice?.index ?? null;
 };
