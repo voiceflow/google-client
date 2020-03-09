e538ad2cadbb926974ded0a816882fd977915296
leartgjoni-voiceflow <leart@voiceflow.com>
2020-03-09 15:27:17 -0400

    test: google service

--- tests/lib/services/state.unit.ts
+++ tests/lib/services/state.unit.ts
@@ -6,35 +6,33 @@ import StateManager from '@/lib/services/state';
 describe('stateManager unit tests', async () => {
   afterEach(() => sinon.restore());
 
-  describe('getDiagram', () => {
-    describe('saveToDb', () => {
-      it('works correctly', async () => {
-        const putPromise = sinon.stub();
-        const services = {
-          docClient: { put: sinon.stub().returns({ promise: putPromise }) },
-        };
-        const config = {
-          SESSIONS_DYNAMO_TABLE: 'sessions_table',
-        };
+  describe('saveToDb', () => {
+    it('works correctly', async () => {
+      const putPromise = sinon.stub();
+      const services = {
+        docClient: { put: sinon.stub().returns({ promise: putPromise }) },
+      };
+      const config = {
+        SESSIONS_DYNAMO_TABLE: 'sessions_table',
+      };
 
-        const stateManager = new StateManager(services as any, config as any);
+      const stateManager = new StateManager(services as any, config as any);
 
-        const userId = 'user-id';
-        const state = { foo: 'bar' };
-        await stateManager.saveToDb(userId, state as any);
+      const userId = 'user-id';
+      const state = { foo: 'bar' };
+      await stateManager.saveToDb(userId, state as any);
 
-        expect(services.docClient.put.args[0]).to.eql([
-          {
-            TableName: config.SESSIONS_DYNAMO_TABLE,
-            Item: {
-              id: `${StateManager.GACTION_SESSIONS_DYNAMO_PREFIX}.${userId}`,
-              state,
-            },
+      expect(services.docClient.put.args[0]).to.eql([
+        {
+          TableName: config.SESSIONS_DYNAMO_TABLE,
+          Item: {
+            id: `${StateManager.GACTION_SESSIONS_DYNAMO_PREFIX}.${userId}`,
+            state,
           },
-        ]);
+        },
+      ]);
 
-        expect(putPromise.callCount).to.eql(1);
-      });
+      expect(putPromise.callCount).to.eql(1);
     });
   });
 });
