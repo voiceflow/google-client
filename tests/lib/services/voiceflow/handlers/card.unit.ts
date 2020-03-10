9e0306d642527e98362a59523b36d82de5f0e486
leartgjoni-voiceflow <leart@voiceflow.com>
2020-03-10 14:28:30 -0400

    test: card handler v2

--- tests/lib/services/voiceflow/handlers/card.unit.ts
+++ tests/lib/services/voiceflow/handlers/card.unit.ts
@@ -1,12 +1,32 @@
 // import { expect } from 'chai';
 import sinon from 'sinon';
 
-import { CardHandlerGenerator } from '@/lib/services/voiceflow/handlers/card';
+import { CardHandlerGenerator, CardResponseBuilder } from '@/lib/services/voiceflow/handlers/card';
 
 describe('card handler unit tests', async () => {
   afterEach(() => sinon.restore());
 
-  describe('handler', () => {
+  describe('canHandle', () => {
+    it('works correctly', async () => {
+      const utils = {
+        addVariables: sinon.stub().returns(''),
+      };
+
+      const cardHandler = CardHandlerGenerator(utils);
+
+      const block = {
+        card: {
+          foo: 'bar',
+        },
+      };
+
+      cardHandler.canHandle(block as any, null as any, null as any, null as any);
+
+      // todo: assertions
+    });
+  });
+
+  describe('handle', () => {
     it('works correctly', async () => {
       const utils = {
         addVariables: sinon.stub().returns(''),
@@ -31,6 +51,19 @@ describe('card handler unit tests', async () => {
       const variables = { foo: 'bar' };
 
       cardHandler.handle(block as any, context as any, variables as any, null as any);
+
+      // todo: assertions
+    });
+  });
+
+  describe('responseBuilder', () => {
+    it('works correctly', async () => {
+      const context = {
+        turn: { get: sinon.stub().returns(null) },
+      };
+      const conv = { add: sinon.stub() };
+
+      CardResponseBuilder(context as any, conv as any);
 
       // todo: assertions
     });
