3588c6fd08c3f9821f76d7d137d38911d04d940c
Tyler Han <tylerhan97@gmail.com>
2020-04-28 17:26:37 -0400

    fix: update request size limit

--- backend/api/routers/google.ts
+++ backend/api/routers/google.ts
@@ -1,12 +1,13 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 
+import { BODY_PARSER_SIZE_LIMIT } from '@/backend/constants';
 import { ControllerMap, MiddlewareMap } from '@/lib';
 
 export default (_middlewares: MiddlewareMap, controllers: ControllerMap) => {
   const router = express.Router();
 
-  router.use(bodyParser.json());
+  router.use(bodyParser.json({ limit: BODY_PARSER_SIZE_LIMIT }));
   router.post('/state/skill/gactions/:versionID', controllers.google.handler);
 
   return router;
