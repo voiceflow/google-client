1b0df2754200e252ca3e0005b185c3f3570dbd48
leartgjoni-voiceflow <leart@voiceflow.com>
2020-05-01 12:14:24 -0400

    wip: add adapter

--- lib/services/state.ts
+++ lib/services/state.ts
@@ -20,6 +20,34 @@ class StateManager extends AbstractManager {
     await docClient.put(params).promise();
   }
 
+  async contextAdapter(state: Record<any, any>) {
+    // eslint-disable-next-line no-console
+    console.log('CONTEXT ADAPTER', state);
+    // eslint-disable-next-line no-process-exit
+    // process.exit(0);
+
+    return {
+      stack: [],
+      storage: {
+        output: state.lastOutput,
+        sessions: state.sessions,
+        locale: state.locale,
+        user: state.user,
+        repeat: state.repeat,
+      },
+      variables: {
+        sessions: state.globals[0].sessions,
+        voiceflow: {
+          events: state.globals[0].voiceflow.events,
+        },
+        locale: state.globals[0].locale,
+        user_id: state.globals[0].user_id,
+        platform: state.globals[0].platform,
+        timestamp: state.globals[0].timestamp,
+      },
+    };
+  }
+
   async getFromDb(userId: string) {
     const { docClient } = this.services;
     const { SESSIONS_DYNAMO_TABLE } = this.config;
@@ -37,7 +65,18 @@ class StateManager extends AbstractManager {
 
     const data = await docClient.get(params).promise();
 
-    return data.Item?.state ?? {};
+    // const item = data.Item ?? {};
+    let rawContext: Record<string, any>;
+    if (data.Item) {
+      rawContext = data.Item.state ? data.Item.state : await this.contextAdapter(data.Item.attributes);
+    } else {
+      rawContext = {};
+    }
+
+    // eslint-disable-next-line no-console
+    console.log('rawContext', rawContext);
+
+    return rawContext;
   }
 }
 
