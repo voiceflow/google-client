fd38e3e404cb0bc5408eeffd3073eb9b57899bc0
leartgjoni-voiceflow <leart@voiceflow.com>
2020-09-23 15:09:11 -0400

    test: stream response builder unit

--- tests/lib/services/voiceflow/handlers/stream.unit.ts
+++ tests/lib/services/voiceflow/handlers/stream.unit.ts
@@ -2,7 +2,13 @@ import { expect } from 'chai';
 import sinon from 'sinon';
 
 import { F, S, T } from '@/lib/constants';
-import DefaultStreamHandler, { StreamHandler, StreamResponseBuilder, StreamResponseBuilderGenerator } from '@/lib/services/voiceflow/handlers/stream';
+import DefaultStreamHandler, {
+  StreamHandler,
+  StreamResponseBuilder,
+  StreamResponseBuilderGenerator,
+  StreamResponseBuilderGeneratorV2,
+  StreamResponseBuilderV2,
+} from '@/lib/services/voiceflow/handlers/stream';
 
 describe('stream handler unit tests', async () => {
   afterEach(() => sinon.restore());
@@ -380,6 +386,129 @@ describe('stream handler unit tests', async () => {
 
         expect(SuggestionsBuilder.args).to.eql([[['continue', 'exit']]]);
         expect(conv.add.callCount).to.eql(2);
+      });
+    });
+  });
+
+  describe('responseBuilderV2', () => {
+    it('no card', async () => {
+      const context = {
+        turn: { get: sinon.stub().returns(null) },
+      };
+
+      StreamResponseBuilderV2(context as any, null as any);
+
+      expect(context.turn.get.args[0]).to.eql([T.STREAM_PLAY]);
+    });
+
+    it('no capabilities', () => {
+      const context = {
+        turn: { get: sinon.stub().returns({}) },
+      };
+
+      const conv = { device: { capabilities: { includes: sinon.stub().returns(false) } }, add: sinon.stub() };
+
+      StreamResponseBuilderV2(context as any, conv as any);
+
+      expect(context.turn.get.args[0]).to.eql([T.STREAM_PLAY]);
+      expect(conv.device.capabilities.includes.callCount).to.eql(1);
+      expect(conv.add.args).to.eql([['Sorry, this device does not support audio playback.']]);
+    });
+
+    describe('has capabilities', () => {
+      it('no background or icon', () => {
+        const MediaObjectBuilder = sinon.stub();
+
+        const streamResponseBuilder = StreamResponseBuilderGeneratorV2(null as any, MediaObjectBuilder as any, null as any);
+
+        const play = { title: 'title', description: 'description', icon_img: null, background_img: null, url: 'url' };
+
+        const turnGet = sinon.stub().returns(true);
+        turnGet.onCall(0).returns(play);
+
+        const context = {
+          turn: { get: turnGet },
+        };
+
+        const conv = { device: { capabilities: { includes: sinon.stub().returns(true) } }, add: sinon.stub() };
+
+        streamResponseBuilder(context as any, conv as any);
+
+        const mediaObject = { name: play.title, description: play.description, url: play.url, image: { icon: undefined, large: undefined } };
+        expect(MediaObjectBuilder.args).to.eql([[{ mediaObjects: [mediaObject], mediaType: 'AUDIO', optionalMediaControls: ['PAUSED', 'STOPPED'] }]]);
+        expect(conv.add.args).to.eql([[{}]]);
+      });
+
+      it('background but no icon', () => {
+        const ImageBuilder = sinon.stub();
+        const MediaObjectBuilder = sinon.stub();
+
+        const streamResponseBuilder = StreamResponseBuilderGeneratorV2(ImageBuilder as any, MediaObjectBuilder as any, null as any);
+
+        const play = { title: 'title', description: 'description', icon_img: null, background_img: 'background', url: 'url' };
+
+        const turnGet = sinon.stub().returns(true);
+        turnGet.onCall(0).returns(play);
+
+        const context = {
+          turn: { get: turnGet },
+        };
+
+        const conv = { device: { capabilities: { includes: sinon.stub().returns(true) } }, add: sinon.stub() };
+
+        streamResponseBuilder(context as any, conv as any);
+
+        expect(ImageBuilder.args).to.eql([[{ url: play.background_img, alt: 'Media Background Image' }]]);
+        const mediaObject = { name: play.title, description: play.description, url: play.url, image: { icon: undefined, large: {} } };
+        expect(MediaObjectBuilder.args).to.eql([[{ mediaObjects: [mediaObject], mediaType: 'AUDIO', optionalMediaControls: ['PAUSED', 'STOPPED'] }]]);
+      });
+
+      it('icon but no background', () => {
+        const ImageBuilder = sinon.stub();
+        const MediaObjectBuilder = sinon.stub();
+
+        const streamResponseBuilder = StreamResponseBuilderGeneratorV2(ImageBuilder as any, MediaObjectBuilder as any, null as any);
+
+        const play = { title: 'title', description: 'description', icon_img: 'icon', background_img: null, url: 'url' };
+
+        const turnGet = sinon.stub().returns(false);
+        turnGet.onCall(0).returns(play);
+        turnGet.onCall(2).returns(true);
+
+        const context = {
+          turn: { get: turnGet },
+        };
+
+        const conv = { device: { capabilities: { includes: sinon.stub().returns(true) } }, add: sinon.stub() };
+
+        streamResponseBuilder(context as any, conv as any);
+
+        expect(ImageBuilder.args).to.eql([[{ url: play.icon_img, alt: 'Media Icon Image' }]]);
+        const mediaObject = { name: play.title, description: play.description, url: play.url, image: { icon: {}, large: undefined } };
+        expect(MediaObjectBuilder.args).to.eql([[{ mediaObjects: [mediaObject], mediaType: 'AUDIO', optionalMediaControls: ['PAUSED', 'STOPPED'] }]]);
+      });
+
+      it('with suggestions', () => {
+        const SuggestionsBuilder = sinon.stub();
+        const MediaObjectBuilder = sinon.stub();
+
+        const streamResponseBuilder = StreamResponseBuilderGeneratorV2(null as any, MediaObjectBuilder as any, SuggestionsBuilder as any);
+
+        const play = { title: 'title', description: 'description', icon_img: null, background_img: null, url: 'url' };
+
+        const turnGet = sinon.stub().returns(false);
+        turnGet.onCall(0).returns(play);
+
+        const context = {
+          turn: { get: turnGet },
+        };
+
+        const conv = { device: { capabilities: { includes: sinon.stub().returns(true) } }, add: sinon.stub() };
+
+        streamResponseBuilder(context as any, conv as any);
+
+        expect(SuggestionsBuilder.args).to.eql([[{ title: 'continue' }], [{ title: 'exit' }]]);
+        expect(conv.add.callCount).to.eql(3);
       });
     });
   });
