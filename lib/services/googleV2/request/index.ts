e9baf187f622bd5d78b6b8d062dd0a99e86f416d
leartgjoni-voiceflow <leart@voiceflow.com>
2020-09-24 15:43:05 -0400

    fix: slot extraction

--- lib/services/googleV2/request/index.ts
+++ lib/services/googleV2/request/index.ts
@@ -9,19 +9,36 @@ import Context from './lifecycle/context';
 import Initialize from './lifecycle/initialize';
 import Response from './lifecycle/response';
 
+const SLOT_FILLING_PREFIX = 'slot_filling_';
 @injectServices({ initialize: Initialize, context: Context, response: Response })
 class HandlerManager extends AbstractManager<{ initialize: Initialize; context: Context; response: Response }> {
+  _extractSlots(conv: ConversationV3) {
+    const handler = conv.request.handler as { originalName: string };
+
+    if (handler.originalName.startsWith(SLOT_FILLING_PREFIX)) {
+      // slot filling - extract slots from scene
+      const rawSlots = conv.scene.slots || {};
+      return Object.keys(rawSlots).reduce((acc, key) => {
+        acc[key] = rawSlots[key].value;
+        return acc;
+      }, {} as Record<string, string>);
+    }
+
+    // intent matching - extract slots from intent
+    const rawSlots = conv.intent.params || {};
+    return Object.keys(rawSlots).reduce((acc, key) => {
+      acc[key] = rawSlots[key].resolved;
+      return acc;
+    }, {} as Record<string, string>);
+  }
+
   async handle(conv: ConversationV3) {
     const { initialize, context, response } = this.services;
 
     const { intent } = conv;
     const input = intent.query;
 
-    const rawSlots = conv.scene.slots || {};
-    const slots = Object.keys(rawSlots).reduce((acc, key) => {
-      acc[key] = rawSlots[key].value;
-      return acc;
-    }, {} as Record<string, string>);
+    const slots = this._extractSlots(conv);
 
     const { userId } = conv.user.params;
 
