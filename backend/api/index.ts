046959fef081b6c95fe86210333f108fc66c0085
Frank Gu <frank.gu968@outlook.com>
2021-04-28 15:07:00 -0700

    fix: health check backwards compatibility

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -7,7 +7,7 @@ import GoogleRouter from './routers/google';
 export default (middlewares: MiddlewareMap, controllers: ControllerMap) => {
   const router = express.Router();
 
-  router.get('/health', (_, res) => res.send(`${process.env.NODE_ENV} healthy`));
+  router.get('/health', (_, res) => res.send(`${process.env.NODE_ENV} Healthy`));
   router.use('/', GoogleRouter(middlewares, controllers));
 
   return router;
