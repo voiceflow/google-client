6ae003db26d0d54025af9a010fad865cfd36ef8f
Jonah Snider <jonah@jonahsnider.com>
2021-06-11 14:23:17 -0700

    feat: use `secure-json-parse` and `@voiceflow/bodyparser`

--- backend/api/routers/dialogflow.ts
+++ backend/api/routers/dialogflow.ts
@@ -1,5 +1,6 @@
-import bodyParser from 'body-parser';
+import bodyParser from '@voiceflow/body-parser';
 import express from 'express';
+import sjson from 'secure-json-parse';
 
 import { BODY_PARSER_SIZE_LIMIT } from '@/backend/constants';
 import { ControllerMap, MiddlewareMap } from '@/lib';
@@ -7,7 +8,7 @@ import { ControllerMap, MiddlewareMap } from '@/lib';
 export default (_middlewares: MiddlewareMap, controllers: ControllerMap) => {
   const router = express.Router();
 
-  router.use(bodyParser.json({ limit: BODY_PARSER_SIZE_LIMIT }));
+  router.use(bodyParser.json({ limit: BODY_PARSER_SIZE_LIMIT, customJSONParser: sjson.parse }));
 
   /**
    * webhook endpoint for dialogflow es agent
